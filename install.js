797479cf37a07e32ed05222d7e997fd8b3a438a9
Martijn Swart <martijn@studiokloek.nl>
2019-09-03 16:30:53 +0200

    Adobe uses DMG for latest AIR SDK om mac (darwin)
    These changes accomodate for the changes in file structure.

--- install.js
+++ install.js
@@ -9,34 +9,57 @@
  * consuming this entire package.
  */
 'use strict';
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const airSdk = require('./lib/air');
 const request = require('request');
 const playerGlobal = require('playerglobal-latest');
-const packageMetadata = require('./package.json');
-const shell = require('shelljs');
 const chalk = require('chalk');
-const name = 'AIRSDK_Compiler.zip';
-const libFolder = 'lib/AIR_SDK';
-const tmpLocation = path.join(__dirname, 'lib', name);
-const frameworksDir = path.join(__dirname, libFolder);
 const progress = require('request-progress');
-const extract = require('extract-zip');
-let downloadUrl;
+const dmg = require('dmg');
+
+const packageMetadata = require('./package.json');
+const name = 'AIRSDK_Compiler';
+const libFolder = 'lib/AIR_SDK';
+const downloadUrl = packageMetadata.airSdk[process.platform];
+const fileExtention = path.extname(downloadUrl);
+const tmpLocation = path.join(__dirname, 'lib', name + fileExtention);
+const frameworksDir = path.join(__dirname, libFolder);
+// const extract = require('extract-zip');
+
+function installAIRSDK() {
+    // display the version number within the archive
+    airSdk.extractVersion();
+
+    // generate a mapping to the tooling binaries in the /bin folder of the AIR SDK
+    airSdk.update();
+
+    // install all the available versions of the playerglobal framework
+    console.log('Installing all playerglobal frameworks...');
+    playerGlobal.install(frameworksDir, function(err) {
+      if (err) {
+        console.error(chalk.magenta('Failed to install the latest "playerglobal.swc" library collection!', err));
+      } else {
+        console.log(chalk.green('Successfully installed the latest "playerglobal.swc" library collection.'));
+      }
+
+      process.exit(err ? 1 : 0);
+    });
+}
 
 fs.stat(libFolder, function(err, stats) {
   if (!err) {
       // TODO - allow for a --force override?
       console.log(chalk.magenta('AIR SDK was already downloaded'));
+
       process.exit(0);
   }
 
-  console.log(chalk.yellow('working on platform - ' + process.platform));
-  downloadUrl = packageMetadata.airSdk[process.platform];
+  console.log(chalk.yellow('Working on platform - ' + process.platform));
   console.log(chalk.yellow(downloadUrl));
 
   console.log(chalk.green('Downloading latest Adobe AIR SDK, please wait...'));
+
   progress(request(downloadUrl, function (error, response, body) {
 
     if (error || response.statusCode !== 200) {
@@ -46,37 +69,38 @@ fs.stat(libFolder, function(err, stats) {
     }
 
     console.log(chalk.yellow('AIR SDK download complete!'));
-    console.log(chalk.yellow('Preparing to extract AIR SDK archive...'));
+    console.log(chalk.yellow('Preparing to extract AIR SDK archive, please wait......'));
 
-    extract(tmpLocation, {dir: frameworksDir}, function (err) {
-      // extraction is complete. make sure to handle the err
-      if (err) {
-        console.error(chalk.magenta('Failed to extract the AIR SDK archive'));
-        process.exit(1);
-      }
-
-      // display the version number within the archive
-      airSdk.extractVersion();
-
-      // generate a mapping to the tooling binaries in the /bin folder of the AIR SDK
-      airSdk.update();
-
-      // install all the available versions of the playerglobal framework
-      console.log('Installing all playerglobal frameworks...');
-      playerGlobal.install(frameworksDir, function(err) {
+    if (fileExtention === '.dmg') {
+      // to open & mount the dmg
+      dmg.mount(tmpLocation, function(err, path) {
         if (err) {
-          console.error(chalk.magenta('Failed to install the latest "playerglobal.swc" library collection!', err));
-        } else {
-          console.log(chalk.green('Successfully installed the latest "playerglobal.swc" library collection.'));
+          console.error(chalk.magenta('Failed to open DMG file...'));
+          process.exit(1);          
         }
 
-        process.exit(err ? 1 : 0);
-      });
+        // show all files in dmg
+        fs.copySync(path, frameworksDir);
 
-    });
+        // later you can and should unmount
+        dmg.unmount(path, function(err) {
+          installAIRSDK();
+        });
+      });      
+    } else {
+      extract(tmpLocation, {dir: frameworksDir}, function (err) {
+        // extraction is complete. make sure to handle the err
+        if (err) {
+          console.error(chalk.magenta('Failed to extract the AIR SDK archive'));
+          process.exit(1);
+        }
+
+        installAIRSDK();    
+      });
+    }
   }))
   .on('progress', function (state) {
-    process.stdout.write(chalk.cyan('Downloading progress: ' + (state.percent * 100).toFixed(3) + ' %\n'));
+    process.stdout.write(chalk.cyan('Downloading: ' + (Math.floor(state.percent * 1000))/10 + '%\n'));
   })
   .pipe(fs.createWriteStream(tmpLocation));
 
